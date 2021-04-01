72abe217e5b16556233eef3d490bc4f13e8f3880
finnolin <finnolin@gmail.com>
2021-04-01 23:35:55 +0200

    changed audio device to 1

--- samplerbox.py
+++ samplerbox.py
@@ -14,7 +14,7 @@
 # CONFIG
 #########################################
 
-AUDIO_DEVICE_ID = 2                     # change this number to use another soundcard
+AUDIO_DEVICE_ID = 1                     # change this number to use another soundcard
 SAMPLES_DIR = "."                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
 USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
